467660f18ab56d8edd8b756f50f6f461d530011c
Vitalii <vitalii.lisovskyi.dev@gmail.com>
2020-10-20 21:19:26 +0300

    fix bug when play several songs in the same time

--- src/pages/ListSongs/ListSongs.js
+++ src/pages/ListSongs/ListSongs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useRef, createRef} from "react";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import {
@@ -10,7 +10,26 @@ import albums from "../../data/mockData";
 const ListSongs =()=>{
     let { id } = useParams();
     const album  = albums.find(x => x.id === parseInt(id));
-    console.log(album)
+    const init = album.songs.map((song)=>{
+        return {
+            playStatus:false,
+            songId:song.id
+        }
+    })
+    const [play,setPlay] = useState(init)
+    const elementsRef = useRef(albums.map(() => createRef()));
+    const onPlayHandler=(id)=>{
+        let newPlay=play;
+        newPlay.forEach((item,index)=>{
+            if(item.songId === id){
+                newPlay[index].playStatus = true
+            }else{
+                newPlay[index].playStatus = false
+                elementsRef.current[index].current.audio.current.pause()
+            }
+        })
+        setPlay(newPlay)
+    }
     return(
         <>
         <h2 className="title-album">{album.name}</h2>
@@ -24,8 +43,9 @@ const ListSongs =()=>{
                                     <h5 className="card-title title-song">{song.name}</h5>
                                     <AudioPlayer
                                         src={song.url}
-                                        onPlay={e => console.log("onPlay")}
+                                        onPlay={() =>onPlayHandler(song.id, elementsRef.current[i])}
                                         style={{backgroundColor:'rgb(3 211 182)'}}
+                                        ref={elementsRef.current[i]}
                                     />
                                 </div>
                             </div>
